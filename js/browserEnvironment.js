5c1e216525d2937867a0bff209a0a9aee77941db
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-17 12:07:40 +0700

    Mencari DOM

---
+++ js/browserEnvironment.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+let nama = prompt("Silahkan masukkan nama anda", "Lorem ipsum");
+console.log("Pesan Saya");
+
+window.alert("Hello World");
+
+alert("Hello World");
+
+function alert(nama) {
+  console.log("Hati-hati, " + nama);
+}
+
+alert("Chewbacca");
+
+window.alert("Chewbacca");
