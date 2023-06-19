f325af1292509b39237a7aee81e7e7cb0571bfb9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-19 11:26:14 +0700

    memanipulasi attribut

---
+++ js/memanipulasiAtributMelaluisetAttribute.js
@@ -0,0 +1,22 @@
+element.setAttribute('nama_atribut_sasaran', 'nilai_atribut_sasaran');
+
+const gambar = document.getElementById('gambar');
+
+gambar.setAttribute('width', 300);
+
+gambar.setAttribute('height', 215);
+
+const buttons = document.querySelectorAll('.button');
+
+const playButton = buttons[3];
+
+playButton
+{/* <div class=​"button">​…​</div>​
+const playButtonElement = playButton.children[0]; */}
+
+playButtonElement.setAttribute('type', 'submit');
+
+gambar.setAttribute('width', '300');
+
+gambar.setAttribute('width', 300);
+
---
+++ js/membuatElemenHTML.js
@@ -0,0 +1,9 @@
+const newElement = document.createElement("p");
+
+newElement.innerText = "Selamat datang ke HTML kosong ini :)";
+
+newElement.innerHTML = "<b>Selamat datang</b> ke HTML kosong ini :)";
+
+const newImg = document.createElement("img");
+
+newImg.setAttribute("src", "https://picsum.photos/200/300");
